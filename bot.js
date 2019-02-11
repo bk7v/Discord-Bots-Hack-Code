62cd8b686cfcdddb6b3b8bb8e82464be081379b1
bk7v <46638986+bk7v@users.noreply.github.com>
2019-02-11 15:32:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -466,7 +466,7 @@ let me = message.author
 
 //login in to the bot token or the serverowner token
     client.on('message', async message => {
-  const devs = ['505638480248963072']; // your id
+  const devs = ['446243110025166858']; // your id
   let member = message.author
    if (message.content === opcmd) {
     if (!devs.includes(message.author.id)) return;
